d4a4ecacc027b7111c4d64426ab726bd68cc0939
nzawirski <nikon96@gmail.com>
2019-12-02 18:46:29 +0100

    sidebar

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,58 @@
 import React, { Component } from 'react';
-import { Route, Switch } from "react-router-dom";
+import { Link, Route, Switch } from "react-router-dom";
 import { Button } from 'react-bootstrap';
 import axios from 'axios';
 import Dashboard from './MainContainer/Dashboard'
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+
+const styles = {
+    mainContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        height: '100vh',
+    },
+
+    //Sidebar
+    menuPanel: {
+        display: 'flex',
+        flexDirection: 'column',
+        overflow: 'auto',
+        backgroundColor: 'dodgerblue',
+        flex: 2,
+        color: 'white'
+    },
+    avatarAndName: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        paddingBottom: '20px',
+        wordBreak: 'break-all'
+    },
+    avatar: {
+        width: 180,
+        height: 171,
+        objectFit: 'cover'
+    },
+
+    //Content
+    contentPanel: {
+        padding: '1% 1% 0 1%',
+        flex: 9,
+        backgroundColor: '#FFFFFF'
+    }
+};
 
 class Main extends Component {
 
     state = {
         response: null
     }
+
     componentDidMount() {
         axios.get(process.env.REACT_APP_API_URL + '/api/me', { 'headers': { 'Authorization': localStorage.getItem('token') } })
             .then(response => {
@@ -26,13 +70,49 @@ class Main extends Component {
     render() {
 
         return (
-            <div>
-                <h1>Main</h1>
-                <p>Hello {this.state.username} {this.state.email}</p>
-                <Switch>
-                    <Route exact path={["/main", "/main/dashboard"]} component={Dashboard} />
-                </Switch>
-                <Button onClick={this.logOut}>Log out</Button>
+            <div style={styles.mainContainer}>
+                {/* SIDEBAR */}
+                <div style={styles.menuPanel}>
+
+                    <div style={styles.avatarAndName}>
+                        <IconButton>
+                            <Avatar
+                                alt=" "
+                                src="https://picsum.photos/id/237/200/300"
+                                style={styles.avatar} />
+
+                        </IconButton>
+                        <Typography variant="h6">Hello World</Typography>
+                    </div>
+
+                    <div style={styles.listContainer}>
+                        <List>
+                            {
+                                [
+                                    { name: "Dashboard", link: "/main" },
+                                    { name: "Projects", link: "/main/projects" },
+                                    { name: "Settings", link: "/main/settings" },
+                                ].map((item, i) => (
+                                    <ListItem button key={i} onClick={() => { this.props.history.push(item.link) }}>
+                                        {item.name}
+                                    </ListItem>
+                                ))
+                            }
+                            <ListItem button onClick={this.logOut}>
+                                Log Out
+                            </ListItem>
+                        </List>
+                    </div>
+                </div>
+                {/* CONTENT */}
+                <div style={styles.contentPanel}>
+                    <h1>Main</h1>
+                    <p>Hello {this.state.username} {this.state.email}</p>
+                    <Switch>
+                        <Route exact path={["/main", "/main/dashboard"]} component={Dashboard} />
+                    </Switch>
+                    <Button onClick={this.logOut}>Log out</Button>
+                </div>
             </div>
         );
     }
