f796937d230df474dcc1101971ec909bc88f6c8e
nzawirski <nikon96@gmail.com>
2019-12-04 16:50:29 +0100

    botched responsive navbar - to be cont.

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,12 +10,14 @@ import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
+import Nav from 'react-bootstrap/Nav'
 
 const styles = {
     mainContainer: {
         display: 'flex',
         flexDirection: 'row',
         height: '100vh',
+        
     },
 
     //Sidebar
@@ -41,10 +43,18 @@ const styles = {
         objectFit: 'cover'
     },
     //Topbar
-
+    navBar: {
+        width: "100%",
+        position: 'absolute',
+        display: 'flex',
+    },
+    navBarItem: {
+        flex: 1
+    },
 
     //Content
     contentPanel: {
+        marginTop: '24px',
         padding: '1% 1% 0 1%',
         flex: 9,
         backgroundColor: '#FFFFFF'
@@ -70,7 +80,7 @@ class Main extends Component {
                 this.setState({ response: response })
                 this.setState({ username: response.data.username })
                 this.setState({ email: response.data.email })
-                console.log(response)
+                //console.log(response)
             })
     }
 
@@ -80,7 +90,7 @@ class Main extends Component {
 
     updateWindowDimensions() {
         this.setState({ width: window.innerWidth, height: window.innerHeight });
-        console.log(this.state.width + " " + this.state.height)
+        //console.log(this.state.width + " " + this.state.height)
     }
 
 
@@ -90,7 +100,7 @@ class Main extends Component {
     }
 
 
-    renderSidebar(props) {
+    renderSidebar() {
         if (this.state.width > 1024) {
             return (
                 <div style={styles.menuPanel}>
@@ -121,13 +131,26 @@ class Main extends Component {
                             }
                             <ListItem button onClick={this.logOut}>
                                 Log Out
-        </ListItem>
+                            </ListItem>
                         </List>
                     </div>
                 </div>
             )
-        }else{
-            return (<h1>topbar should appear</h1>)
+        } else {
+            return (
+                <Nav variant="pills" activeKey={window.location.pathname} style={styles.navBar}>
+                    {
+                        [
+                            { name: "Dashboard", link: "/main" },
+                            { name: "Projects", link: "/main/projects" },
+                            { name: "Settings", link: "/main/settings" },
+                        ].map((item, i) => (
+                            <Nav.Item key={i} style={styles.navBarItem}>
+                                <Nav.Link eventKey={item.link} onClick={() => { this.props.history.push(item.link) }}>{item.name}</Nav.Link>
+                            </Nav.Item>
+                        ))
+                    }
+                </Nav>)
         }
     }
 
@@ -136,7 +159,7 @@ class Main extends Component {
         return (
             <div style={styles.mainContainer}>
                 {/* SIDEBAR */}
-                <this.renderSidebar/>
+                <this.renderSidebar />
                 {/* CONTENT */}
                 <div style={styles.contentPanel}>
                     <h1>Main</h1>
